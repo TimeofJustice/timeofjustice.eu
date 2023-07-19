8ac6def61b91053c3d8a9479cbf616d266963447
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2023-07-20 00:24:21 +0200

    Update settings.py

--- timeofjustice/timeofjustice/settings.py
+++ timeofjustice/timeofjustice/settings.py
@@ -32,6 +32,14 @@ ALLOWED_HOSTS = [
     'localhost',
     'timeofjustice.eu',
 ]
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'https://timeofjustice.eu',
+    'http://timeofjustice.eu',
+    'http://localhost',
+    'https://172.67.128.248:443'
+]
 
 
 # Application definition
@@ -44,11 +52,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
