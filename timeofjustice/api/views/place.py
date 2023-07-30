239fc5744414529401249f8a07298c674977119a
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2023-07-30 03:38:51 +0200

    Update place.py

--- timeofjustice/api/views/place.py
+++ timeofjustice/api/views/place.py
@@ -30,13 +30,13 @@ def rgb_to_hex(rgb):
     return '#{0:02x}{1:02x}{2:02x}'.format(rgb[0], rgb[1], rgb[2])
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='60/m')
+@ratelimit(key='ip', rate='60/m')
 def get_time_out(request):
     time_out = models.PlaceTimeOut.objects.all()[0].seconds
     return JsonResponse({"seconds": time_out}, safe=False)
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='60/m')
+@ratelimit(key='ip', rate='60/m')
 def get_last_placed(request):
     timeout_in_seconds = models.PlaceTimeOut.objects.all()[0].seconds
     session_id = request.COOKIES.get("session")
@@ -62,10 +62,11 @@ def get_last_placed(request):
         return JsonResponse({"seconds": seconds}, safe=False)
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='120/m')
+@ratelimit(key='ip', rate='120/m')
 @ensure_csrf_cookie
 def place_set(request):
     session_id = request.COOKIES.get("session")
+    print(request.META.get["REMOTE_ADDR"])
 
     if session_id is None:
         return JsonResponse({"error": "Missing session id"}, status=400)
@@ -118,7 +119,7 @@ def place_set(request):
     return JsonResponse({"x": cell.x, "y": cell.y, "color": cell.color}, safe=False)
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='160/m')
+@ratelimit(key='ip', rate='160/m')
 def gen(request, from_x, from_y):
     if from_x < 0 or from_y < 0 or from_x > 1000 or from_y > 1000:
         return HttpResponse("Wrong coordinates")
@@ -194,7 +195,7 @@ def changes(request, from_x, from_y):
     return response
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='30/m')
+@ratelimit(key='ip', rate='30/m')
 def export(request, from_x, from_y, to_x, to_y, factor=1):
     if from_x < 0 or from_y < 0 or from_x > 1000 or from_y > 1000:
         return HttpResponse("Wrong coordinates")
@@ -227,7 +228,7 @@ def export(request, from_x, from_y, to_x, to_y, factor=1):
     return response
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='60/m')
+@ratelimit(key='ip', rate='60/m')
 def validate_captcha(request):
     if request.method == "POST":
         response = {}
@@ -250,7 +251,7 @@ def validate_captcha(request):
         return JsonResponse(response, safe=False)
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='50/m')
+@ratelimit(key='ip', rate='50/m')
 def get_overlay(request, overlay_name):
     json_images = []
     overlays = models.Overlay.objects.filter(name=overlay_name)
@@ -311,7 +312,7 @@ def discover(request):
     return JsonResponse(neue_dateien, safe=False)
 
 
-@ratelimit(key='header:HTTP_X_FORWARDED_FOR', rate='600/m')
+@ratelimit(key='ip', rate='600/m')
 def get_color(request, x, y):
     cells = models.Cell.objects.filter(x=x, y=y)
 
