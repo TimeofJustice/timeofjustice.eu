b6b563cf600333e15f527ed6241335966bd3a16d
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2024-08-21 07:16:25 +0200

    Change assets-directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     server: {
       port: 3000,
     },
-    build: { manifest: true, outDir: 'dist', assetsDir: 'static' },
+    build: { manifest: true, outDir: 'dist', assetsDir: 'assets' },
 })
