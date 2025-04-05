121650cb17f7c8ed87fdc3d24c28738e20ccc6d7
Jonas <83582119+TimeofJustice@users.noreply.github.com>
2025-04-05 12:48:23 +0200

    Enhance carousel component with video support and fullscreen (#41)

--- frontend/src/components/Carousel.vue
+++ frontend/src/components/Carousel.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
-import 'vue3-carousel/carousel.css'
-import { Carousel, Slide } from 'vue3-carousel'
+import { onBeforeUnmount, ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
 import { TranslatedText } from "@/types/TranslatedText.ts";
 import { ProjectImage } from "@/types/ProjectImage.ts";
-import { ref } from "vue";
-import { faArrowLeft, faArrowRight, faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faArrowRight, faMaximize, faPlayCircle, faClose } from "@fortawesome/free-solid-svg-icons";
 
 interface CarouselProps {
   items: ProjectImage[];
 }
 
-defineProps<CarouselProps>()
-
-const currentSlide = ref(0)
-const mouseOver = ref(false)
-const videoPlaying = ref(false)
-
-const slideTo = (nextSlide: number) => (currentSlide.value = nextSlide)
-
-// Simulates the autoplay feature to reduce the jumping effect from the thumbnails
-setInterval(() => {
-  if (!mouseOver.value && !videoPlaying.value) slideTo((currentSlide.value + 1))
-}, 5000);
+const { items } = defineProps<CarouselProps>();
+const currentSlide = ref(0);
+const currentItem = ref<ProjectImage | null>(items.length > 0 ? items[0] : null);
+const isMouseOver = ref(false);
+const isFullscreenOpen = ref(false);
 
 const galleryConfig = {
   itemsToShow: 1,
@@ -29,26 +21,80 @@ const galleryConfig = {
   mouseDrag: false,
   touchDrag: false,
   height: 320,
-  slideEffect: 'fade' as const,
-}
+  slideEffect: "fade" as const
+};
 
 const thumbnailsConfig = {
   height: 80,
   itemsToShow: 6,
   wrapAround: true,
   touchDrag: false,
-  gap: 10,
-}
+  gap: 10
+};
+
+const stopVideo = (el: HTMLVideoElement) => {
+  if (!el) return;
+
+  el.pause();
+  el.currentTime = 0;
+};
+
+const slideTo = (nextSlide: number) => {
+  currentSlide.value = nextSlide;
+  currentItem.value = items[nextSlide % items.length];
+
+  const prevVideo = document.getElementById("carousel-player-" + ((currentSlide.value - 1 + items.length) % items.length)) as HTMLVideoElement;
+  stopVideo(prevVideo);
+};
+
+const isVideoPlaying = () => {
+  const video = document.getElementById("carousel-player-" + currentSlide.value % items.length) as HTMLVideoElement;
+  return video && video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2;
+};
+
+const nextSlide = () => {
+  if (!isMouseOver.value && !isVideoPlaying() && !isFullscreenOpen.value) slideTo((currentSlide.value + 1));
+};
+
+const autoSlideInterval = ref(setInterval(nextSlide, 5000));
+
+onBeforeUnmount(() => {
+  clearInterval(autoSlideInterval.value);
+});
+
+const openFullscreen = () => {
+  isFullscreenOpen.value = true;
+
+  stopVideo(document.getElementById("carousel-player-" + currentSlide.value % items.length) as HTMLVideoElement);
+};
+
+const closeFullscreen = () => {
+  const video = document.getElementById("modal-player") as HTMLVideoElement;
+
+  stopVideo(video);
+};
+
+const forceSlideUpdate = (index: number) => {
+  slideTo(index);
+
+  clearInterval(autoSlideInterval.value);
+  autoSlideInterval.value = setInterval(nextSlide, 5000);
+};
 </script>
 
 <template>
-  <div v-if="items.length" @mouseover="mouseOver = true" @mouseleave="mouseOver = false">
+  <div v-if="items.length" @mouseover="isMouseOver = true" @mouseleave="isMouseOver = false">
     <Carousel v-bind="galleryConfig" v-model="currentSlide" class="mb-2">
       <Slide v-for="(image, i) in items" :key="i">
-        <v-lazy-image class="gallery-image" :src="image.image.original" :src-placeholder="image.image.lazy" :alt="image.alt[$i18n.locale as keyof TranslatedText]" v-if="!image.video" />
-        <div class="gallery-image position-relative" v-else>
-          <video :src="image.video" muted controls autoplay playsinline @play="videoPlaying = true" @pause="videoPlaying = false" />
+        <v-lazy-image class="slide gallery" :src="image.image.original" :src-placeholder="image.image.lazy" :alt="image.alt[$i18n.locale as keyof TranslatedText]"
+                      v-if="!image.video" />
+        <div class="slide gallery" v-else>
+          <video :src="image.video" autoplay muted controls playsinline :id="`carousel-player-` + i" />
         </div>
+
+        <BButton variant="secondary" class="btn-circle position-absolute top-0 end-0 m-2" @click="openFullscreen()">
+          <font-awesome-icon :icon="faMaximize" />
+        </BButton>
       </Slide>
     </Carousel>
 
@@ -58,72 +104,41 @@ const thumbnailsConfig = {
           <div
             :class="['thumbnail', { 'is-active': isActive && !isClone }]"
             class="position-relative"
-            @click="slideTo(currentIndex)"
+            @click="forceSlideUpdate(currentIndex)"
             :id="currentIndex"
           >
             <div class="position-absolute z-1 w-100 h-100 d-flex justify-content-center align-items-center fs-2 text-dark" v-if="image.video">
-              <font-awesome-icon :icon="faPlayCircle"></font-awesome-icon>
+              <font-awesome-icon :icon="faPlayCircle" class="text-light opacity-50"></font-awesome-icon>
             </div>
-            <v-lazy-image class="thumbnail-image" :src="image.image.original" :src-placeholder="image.image.lazy" :alt="image.alt[$i18n.locale as keyof TranslatedText]" />
+            <v-lazy-image class="slide thumbnail-image" :src="image.image.original" :src-placeholder="image.image.lazy"
+                          :alt="image.alt[$i18n.locale as keyof TranslatedText]" />
           </div>
         </template>
       </Slide>
 
       <template #addons>
-        <BButton variant="secondary" class="btn-circle ms-1 carousel__prev" @click="slideTo(currentSlide - 1)">
+        <BButton variant="secondary" class="btn-circle ms-1 carousel__prev" @click="forceSlideUpdate(currentSlide - 1)">
           <font-awesome-icon :icon="faArrowLeft" />
         </BButton>
-        <BButton variant="secondary" class="btn-circle me-1 carousel__next" @click="slideTo(currentSlide + 1)">
+        <BButton variant="secondary" class="btn-circle me-1 carousel__next" @click="forceSlideUpdate(currentSlide + 1)">
           <font-awesome-icon :icon="faArrowRight" />
         </BButton>
       </template>
     </Carousel>
   </div>
+
+  <BModal data-bs-theme="dark" v-model="isFullscreenOpen" header-class="justify-content-end" body-class="overflow-hidden d-flex justify-content-center"
+          :hide-footer="true" :no-close-on-backdrop="true" scrollable :no-close-on-esc="true" size="xl" centered @close="closeFullscreen()">
+    <div class="rounded d-flex">
+      <v-lazy-image class="img-fluid rounded" :src="currentItem?.image.original" :src-placeholder="currentItem?.image.lazy"
+                    :alt="currentItem?.alt[$i18n.locale as keyof TranslatedText]" v-if="!currentItem?.video" />
+      <video class="img-fluid rounded" :src="currentItem?.video" controls playsinline id="modal-player" v-else />
+    </div>
+
+    <template #header>
+      <BButton variant="tertiary" class="btn-square text-light" @click="isFullscreenOpen = false">
+        <font-awesome-icon :icon="faClose" />
+      </BButton>
+    </template>
+  </BModal>
 </template>
-
-<style scoped lang="scss">
-:root {
-  background-color: #242424;
-}
-
-.carousel {
-  --vc-nav-background: rgba(255, 255, 255, 0.7);
-  --vc-nav-border-radius: 100%;
-}
-
-img {
-  border-radius: 8px;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.gallery-image {
-  border-radius: 16px;
-  overflow: hidden;
-  width: 100%;
-  height: 100%;
-}
-
-.gallery-image > video {
-  border-radius: 16px;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.thumbnail {
-  height: 95%;
-  width: 95%;
-  cursor: pointer;
-  opacity: 0.6;
-  transition: opacity 0.3s ease-in-out, height 0.3s ease-in-out, width 0.3s ease-in-out;
-}
-
-.thumbnail.is-active,
-.thumbnail:hover {
-  opacity: 1;
-  height: 100%;
-  width: 100%;
-}
-</style>
