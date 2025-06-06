0e30d5bad1076bc3a884937b319d30d0e004adbf
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2025-06-06 18:12:15 +0200

    Add wss

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", f'backend.settings')
-django.setup()
-application = get_default_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django_asgi_app = get_asgi_application()
+
+from r_place.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
