97dd1e6649abe431f5de018bee0e60eb81f008bb
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2025-06-28 18:43:53 +0200

    Improve production container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 backend.wsgi:application &
-daphne -b 0.0.0.0 -p 8001 backend.asgi:application
+gunicorn --bind unix:/sockets/gunicorn.sock backend.wsgi:application &
+daphne -b 0.0.0.0 -p 8000 backend.asgi:application
