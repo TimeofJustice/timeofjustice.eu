93c2d94f207e6d565fefdf28bb8255fafefd6e1c
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2025-07-10 17:03:17 +0200

    Fix staging redis host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(os.getenv('REDIS_HOST', 'redis'), 6379)],
         },
     },
 }
