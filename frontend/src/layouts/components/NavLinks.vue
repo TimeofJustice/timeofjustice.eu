e0b69328e52fa22e95b97e22c0d9f4addf8a4a4c
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2025-07-23 14:46:11 +0200

    Restyle navbar offcanvas

---
+++ frontend/src/layouts/components/NavLinks.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { faDice, faHome, faPaintBrush } from "@node_modules/@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@node_modules/@fortawesome/vue-fontawesome";
+</script>
+
+<template>
+  <BNavItem to="/" link-class="d-flex align-items-center" :class="{ 'active': $page.component === 'Projects' }">
+    <FontAwesomeIcon :icon="faHome"/>
+    <div class="ms-1 link-title">{{ $t('nav.projects') }}</div>
+  </BNavItem>
+  <BNavItem to="/games/" link-class="d-flex align-items-center" :class="{ 'active': $page.component === 'Games/Main' || $page.component === 'Games/Entry' || $page.component === 'Games/Login' }">
+    <FontAwesomeIcon :icon="faDice"/>
+    <div class="ms-1 link-title">{{ $t('nav.games') }}</div>
+  </BNavItem>
+  <BNavItem to="/r-place/" link-class="d-flex align-items-center position-relative" :class="{ 'active': $page.component === 'RPlace' }">
+    <FontAwesomeIcon :icon="faPaintBrush"/>
+    <div class="ms-1 link-title">{{ $t('nav.place') }}</div>
+    <BBadge
+      variant="danger"
+      class="position-absolute top-0 start-100 translate-middle mt-1 me-1 border-0 pulse"
+      dot-indicator
+    >
+    </BBadge>
+  </BNavItem>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- frontend/src/layouts/components/Navbar.vue
+++ frontend/src/layouts/components/Navbar.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { faBars, faClose, faDice, faHome, faPaintBrush } from "@node_modules/@fortawesome/free-solid-svg-icons";
+import { faBars } from "@node_modules/@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@node_modules/@fortawesome/vue-fontawesome";
 import LocaleDropdown from "@components/LocaleDropdown.vue";
 import { onMounted, ref } from "vue";
+import NavLinks from "@layouts/components/NavLinks.vue";
 
 interface IBasicLayout {
   small?: boolean;
@@ -23,51 +24,44 @@ onMounted(() => {
 <template>
   <div class="navbar navbar-expand-lg top-0 z-1 w-100 d-flex align-content-center justify-content-center pe-none" :class="{ 'position-absolute': small, 'position-sticky': !small }">
     <div class="container-xxl navbar-body pe-auto" :class="{ 'scrolled blur-box': isScrolled || small}">
-      <BButton variant="tertiary" class="btn-square navbar-toggler border-0 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+      <BNavbarToggle target="nav-offcanvas" class="btn btn-tertiary btn-square navbar-toggler border-0 text-white">
         <FontAwesomeIcon :icon="faBars"/>
-      </BButton>
+      </BNavbarToggle>
 
-      <div class="navbar-brand d-flex me-0 me-lg-3">
+      <BNavbarBrand class="d-flex me-0 me-lg-3">
         <LocaleDropdown class="d-block d-lg-none"/>
-
         <img class="img-fluid rounded" :src="require('/assets/images/TimeofJustice.svg')" style="width: 2.3rem; min-width: 2.3rem; height: 2.3rem; min-height: 2.3rem;" alt="Time of Justice Logo"/>
-      </div>
+      </BNavbarBrand>
 
-      <div class="navbar-nav offcanvas offcanvas-start offcanvas-small d-flex justify-content-between flex-lg-row" id="offcanvasNavbar">
-        <div class="offcanvas-header">
-          <BButton variant="tertiary" class="btn-square ms-0" type="button" data-bs-dismiss="offcanvas">
-            <font-awesome-icon :icon="faClose"/>
-          </BButton>
+      <BNavbarNav class="d-none d-lg-flex justify-content-between w-100">
+        <div class="d-flex align-items-center ps-0 pt-0">
+          <NavLinks />
         </div>
 
-        <div class="offcanvas-body d-flex flex-column flex-lg-row align-items-lg-center ps-3 ps-lg-0 pt-2 pt-lg-0">
-          <BLink class="nav-item nav-link p-0 px-2 d-flex align-items-center" to="/" :class="{ 'active': $page.component === 'Projects' }">
-            <FontAwesomeIcon :icon="faHome"/>
-            <div class="ms-1 link-title">{{ $t('nav.projects') }}</div>
-          </BLink>
-          <BLink class="nav-item nav-link p-0 px-2 d-flex align-items-center" to="/games/" :class="{ 'active': $page.component === 'Games/Main' || $page.component === 'Games/Entry' || $page.component === 'Games/Login' }">
-            <FontAwesomeIcon :icon="faDice"/>
-            <div class="ms-1 link-title">{{ $t('nav.games') }}</div>
-          </BLink>
-          <BLink class="nav-item nav-link p-0 px-2 position-relative d-flex align-items-center" to="/r-place/" :class="{ 'active': $page.component === 'RPlace' }">
-            <FontAwesomeIcon :icon="faPaintBrush"/>
-            <div class="ms-1 link-title">{{ $t('nav.place') }}</div>
-            <BBadge
-              variant="danger"
-              class="position-absolute top-0 start-100 translate-middle mt-1 me-1 border-0 pulse"
-              dot-indicator
-            >
-            </BBadge>
-          </BLink>
-        </div>
-
-        <LocaleDropdown class="d-none d-lg-block"/>
-      </div>
+        <LocaleDropdown/>
+      </BNavbarNav>
     </div>
+
+    <BOffcanvas
+      id="nav-offcanvas"
+      placement="start"
+      is-nav
+      class="offcanvas-small pe-auto"
+      header-close-class="btn btn-tertiary btn-square ms-0"
+      :teleport-disabled="true"
+    >
+      <template #header-close>
+        <font-awesome-icon icon="fa-solid fa-times" />
+      </template>
+
+      <BNavbarNav>
+        <NavLinks />
+      </BNavbarNav>
+    </BOffcanvas>
   </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 .pulse {
   animation: pulse 2s infinite;
   padding: 0.3rem!important;
@@ -94,6 +88,13 @@ onMounted(() => {
 
   @media (min-width: 1400px) {
     transition: width 0.8s cubic-bezier(0.68, -0.55, 0.27, 1.55), backdrop-filter 0.3s ease-in-out, background 0.3s ease-in-out, box-shadow 0.3s ease-in-out, padding 0.3s ease-in-out, margin 0.3s ease-in-out;
+
+    .link-title {
+      transition: max-width 0.3s ease-in-out;
+      max-width: 10rem;
+      overflow: hidden;
+      transition-delay: 0.2s;
+    }
   }
 }
 
@@ -111,19 +112,21 @@ onMounted(() => {
   @media (max-width: 1400px) {
     width: 100%;
   }
+
+  @media (min-width: 1400px) {
+    .link-title {
+      max-width: 0;
+      transition-delay: 0s;
+    }
+  }
 }
 
-@media (min-width: 1400px) {
-  .navbar-body .link-title {
-    transition: max-width 0.3s ease-in-out;
-    max-width: 10rem;
-    overflow: hidden;
-    transition-delay: 0.2s;
-  }
+.offcanvas .offcanvas-header, .offcanvas .offcanvas-body {
+  --bs-gutter-x: 1.5rem;
 
-  .navbar-body.scrolled .link-title {
-    max-width: 0;
-    transition-delay: 0s;
-  }
+  --bs-offcanvas-padding-y: calc(var(--bs-gutter-x) * 0.5);
+  --bs-offcanvas-padding-x: calc(var(--bs-gutter-x) * 0.5);
+
+  padding-right: 1rem;
 }
 </style>
