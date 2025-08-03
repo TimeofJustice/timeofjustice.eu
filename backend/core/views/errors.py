9f71bfe5d37a23fde6f061aa2d0e4dbae8cafdcf
Jonas Oelschner <jonas.oelschner@stud.hs-flensburg.de>
2025-08-03 13:49:09 +0200

    New error handling

--- backend/core/views/errors.py
+++ backend/core/views/errors.py
@@ -1,17 +1,14 @@
-from django.http import HttpResponseRedirect
-
-
-def page_not_found(request, *args, **kwargs):
-    return HttpResponseRedirect('/error/404')
-
-
-def server_error(request, *args, **kwargs):
-    return HttpResponseRedirect('/error/500')
-
-
-def permission_denied(request, *args, **kwargs):
-    return HttpResponseRedirect('/error/403')
+from core.views import pages
 
 
 def bad_request(request, *args, **kwargs):
-    return HttpResponseRedirect('/error/400')
+    return pages.error(request, 400)
+
+def permission_denied(request, *args, **kwargs):
+    return pages.error(request, 403)
+
+def page_not_found(request, *args, **kwargs):
+    return pages.error(request, 404)
+
+def server_error(request, *args, **kwargs):
+    return pages.error(request, 500)
