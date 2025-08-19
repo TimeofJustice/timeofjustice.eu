2745749e7144dc77bfe83dbb30eabefd04405fae
Jonas <83582119+TimeofJustice@users.noreply.github.com>
2025-08-19 17:09:52 +0200

    Enhance linting and formatting configuration (#135)

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    ignores: ["dist/**"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
