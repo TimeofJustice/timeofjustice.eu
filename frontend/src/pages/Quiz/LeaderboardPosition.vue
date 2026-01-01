88b3b1f8405d048cad66f3ae890670370ad941d0
Jonas <og.timeofjustice@gmail.com>
2026-01-01 18:00:26 +0100

    Add score

--- frontend/src/pages/Quiz/LeaderboardPosition.vue
+++ frontend/src/pages/Quiz/LeaderboardPosition.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
 interface LeaderboardPositionProps {
   name: string;
   url: string;
+  score?: number;
 }
 
-const props = defineProps<LeaderboardPositionProps>();
+const { score = 0 } = defineProps<LeaderboardPositionProps>();
+
+const animatedScore = ref(score);
+const duration = 500;
+
+watch(
+  () => score,
+  (newScore) => {
+    const start = animatedScore.value;
+    const diff = newScore - start;
+    const startTime = performance.now();
+
+    function animate(now: number) {
+      const progress = Math.min((now - startTime) / duration, 1);
+      animatedScore.value = Math.round(start + diff * easeOutCubic(progress));
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    }
+
+    requestAnimationFrame(animate);
+  },
+);
+
+function easeOutCubic(t: number) {
+  return 1 - Math.pow(1 - t, 3);
+}
 </script>
 
 <template>
@@ -15,7 +45,10 @@ const props = defineProps<LeaderboardPositionProps>();
           <img :src="url" />
         </span>
       </BAvatar>
-      {{ props.name }}
+      <div class="d-flex flex-grow-1 justify-content-between">
+        <span>{{ name }}</span>
+        <span>{{ animatedScore }}</span>
+      </div>
     </BCard>
   </div>
 </template>
--- frontend/src/pages/Quiz/LobbyPage.vue
+++ frontend/src/pages/Quiz/LobbyPage.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { shuffle as _shuffle } from "lodash-es";
 import { Head } from "@inertiajs/vue3";
 import { BAvatar } from "bootstrap-vue-next";
 import { ref } from "vue";
@@ -9,22 +8,33 @@ import LeaderboardPosition from "./LeaderboardPosition.vue";
 interface Position {
   id: number;
   name: string;
+  score: number;
 }
 
 const items = ref<Position[]>([
-  { id: 0, name: "Alice" },
-  { id: 1, name: "Bob" },
+  { id: 0, name: "Alice", score: 0 },
+  { id: 1, name: "Bob", score: 0 },
 ]);
 let nextId = 2;
 
 function insert() {
-  const i = Math.round(Math.random() * items.value.length);
-  items.value.splice(i, 0, { id: nextId++, name: "Test" });
+  items.value.splice(items.value.length, 0, {
+    id: nextId++,
+    name: "Test",
+    score: 0,
+  });
   resyncGifs();
 }
 
 function shuffle() {
-  items.value = _shuffle(items.value);
+  items.value.forEach((item) => {
+    const delta = Math.floor(Math.random() * 50) + 1; // +1 bis +50
+    item.score += delta;
+  });
+
+  requestAnimationFrame(() => {
+    items.value.sort((a, b) => b.score - a.score);
+  });
 }
 
 function remove(item: Position) {
@@ -34,123 +44,165 @@ function remove(item: Position) {
   }
 }
 
-// z.B. in Parent (Leaderboard)
 const gifSyncKey = ref(Date.now());
 
 function resyncGifs() {
   gifSyncKey.value = Date.now();
+}
+
+const showLeaderboard = ref(false);
+
+function toggleLeaderboard() {
+  showLeaderboard.value = !showLeaderboard.value;
 }
 </script>
 
 <template>
   <Head :title="$t('quiz.title')" />
 
-  <div
-    class="container d-flex flex-column align-items-stretch gap-2 col-12 col-lg-6"
-  >
-    <BCard>
-      Ich bin eine Antwort, oder vielleicht doch zwei
-
-      <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
-        <BAvatarGroup size="2.3rem" overlap="0.4" class="reverse hover-focus">
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTA2NnF5YWViY25sdDJhb3Q2OTVrdzNmY2ptNnI3YTl2N3Fta2NvaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Dg4TxjYikCpiGd7tYs/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-        </BAvatarGroup>
-      </div>
-    </BCard>
-    <BCard>
-      Ich bin eine Antwort, oder vielleicht doch zwei
-
-      <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
-        <BAvatarGroup size="2.3rem" overlap="0.4" class="reverse hover-focus">
-          <BAvatar text="+4" class="p-0 border border-2 border-black" />
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img src="https://i.imgur.com/lkWFHkU.gif" />
-            </span>
-          </BAvatar>
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTA2NnF5YWViY25sdDJhb3Q2OTVrdzNmY2ptNnI3YTl2N3Fta2NvaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Dg4TxjYikCpiGd7tYs/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjZydWhxdmZqZWtjendmNDZnNHg4bHdlcTdrNG43N3RvYWVoa3pqYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NsKiCmWdA96V4w10N5/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-        </BAvatarGroup>
-      </div>
-    </BCard>
-    <BCard variant="success">
-      Ich bin eine Antwort, oder vielleicht doch zwei
-
-      <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
-        <BAvatarGroup size="2.3rem" overlap="0.4" class="reverse hover-focus">
-        </BAvatarGroup>
-      </div>
-    </BCard>
-    <BCard>
-      Ich bin eine Antwort, oder vielleicht doch zwei
-
-      <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
-        <BAvatarGroup size="2.3rem" overlap="0.4" class="reverse hover-focus">
-          <BAvatar class="p-0 border border-2 border-black">
-            <span class="b-avatar-img position-absolute">
-              <img
-                :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
-              />
-            </span>
-          </BAvatar>
-        </BAvatarGroup>
-      </div>
-    </BCard>
-  </div>
+  <BButton class="container-xxl" @click="toggleLeaderboard">
+    Leaderboard anzeigen
+  </BButton>
 
   <div
-    class="mt-2 container col-12 col-lg-6 d-flex flex-column align-items-stretch gap-2"
+    class="container-xxl flex-grow-1 overflow-x-hidden d-flex justify-content-center position-relative"
   >
-    <BButton @click="insert">Add</BButton>
-    <BButton @click="shuffle">Shuffle</BButton>
-  </div>
+    <Transition name="fade">
+      <div
+        class="d-flex flex-column justify-content-center gap-2 col-6 position-absolute h-100"
+        v-if="!showLeaderboard"
+      >
+        <BCard class="mb-2" variant="secondary">
+          Ich stelle dir die Frage des Lebens? Ich stelle dir die Frage des
+          Lebens? Ich stelle dir die Frage des Lebens?
+        </BCard>
+        <BCard>
+          Ich bin eine Antwort, oder vielleicht doch zwei
 
-  <div class="mt-2 container d-flex flex-column gap-2 col-12 col-lg-6">
-    <TransitionGroup
-      tag="div"
-      name="leaderboard-fade"
-      class="d-flex flex-column gap-2 position-relative"
-    >
-      <LeaderboardPosition
-        v-for="item in items"
-        :key="item.id"
-        :name="item.name"
-        @click="remove(item)"
-        :url="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
-      />
-    </TransitionGroup>
+          <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
+            <BAvatarGroup
+              size="2.3rem"
+              overlap="0.4"
+              class="reverse hover-focus"
+            >
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTA2NnF5YWViY25sdDJhb3Q2OTVrdzNmY2ptNnI3YTl2N3Fta2NvaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Dg4TxjYikCpiGd7tYs/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+            </BAvatarGroup>
+          </div>
+        </BCard>
+        <BCard>
+          Ich bin eine Antwort, oder vielleicht doch zwei
+
+          <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
+            <BAvatarGroup
+              size="2.3rem"
+              overlap="0.4"
+              class="reverse hover-focus"
+            >
+              <BAvatar text="+4" class="p-0 border border-2 border-black" />
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img src="https://i.imgur.com/lkWFHkU.gif" />
+                </span>
+              </BAvatar>
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTA2NnF5YWViY25sdDJhb3Q2OTVrdzNmY2ptNnI3YTl2N3Fta2NvaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Dg4TxjYikCpiGd7tYs/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjZydWhxdmZqZWtjendmNDZnNHg4bHdlcTdrNG43N3RvYWVoa3pqYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NsKiCmWdA96V4w10N5/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+            </BAvatarGroup>
+          </div>
+        </BCard>
+        <BCard variant="success">
+          Ich bin eine Antwort, oder vielleicht doch zwei
+
+          <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
+            <BAvatarGroup
+              size="2.3rem"
+              overlap="0.4"
+              class="reverse hover-focus"
+            >
+            </BAvatarGroup>
+          </div>
+        </BCard>
+        <BCard>
+          Ich bin eine Antwort, oder vielleicht doch zwei
+
+          <div class="position-absolute top-0 w-100 translate-middle-y pe-4">
+            <BAvatarGroup
+              size="2.3rem"
+              overlap="0.4"
+              class="reverse hover-focus"
+            >
+              <BAvatar class="p-0 border border-2 border-black">
+                <span class="b-avatar-img position-absolute">
+                  <img
+                    :src="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
+                  />
+                </span>
+              </BAvatar>
+            </BAvatarGroup>
+          </div>
+        </BCard>
+      </div>
+
+      <div
+        class="d-flex flex-column justify-content-center gap-2 col-4 position-absolute h-100"
+        v-else-if="showLeaderboard"
+      >
+        <span class="fs-1 fw-bold text-center">Leaderboard</span>
+
+        <div class="d-flex flex-column align-items-stretch gap-2">
+          <BButton @click="insert">Add</BButton>
+          <BButton @click="shuffle">Shuffle</BButton>
+        </div>
+
+        <div class="d-flex flex-column gap-2">
+          <TransitionGroup
+            tag="div"
+            name="leaderboard-fade"
+            class="d-flex flex-column gap-2 position-relative"
+          >
+            <LeaderboardPosition
+              v-for="item in items"
+              :key="item.id"
+              :name="item.name"
+              :score="item.score"
+              @click="remove(item)"
+              :url="`https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnppNjQzdHQzcjJjdjF6OHRlNTN5bDh3OWtlb3o0ZzZkeWY4ODZ0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/InqLhljsiZrn1Bdhzq/giphy.gif?t=${gifSyncKey}`"
+            />
+          </TransitionGroup>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -178,5 +230,23 @@ function resyncGifs() {
 .leaderboard-fade-leave-active {
   height: 0%;
   width: 100%;
+}
+
+/* Fade für Umschalten zwischen Frage & Leaderboard */
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.4s ease,
+    transform 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
 }
 </style>
